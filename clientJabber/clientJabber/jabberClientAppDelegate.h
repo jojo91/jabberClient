9ac29f25e4108c9b7683eae250427063ef236abe
Jonathan LEQUEUX <lequeu_j@etna-alternance.net>
2015-05-29 18:51:06 +0200

    Ajout du projet avec une meilleur arborescence

---
+++ clientJabber/clientJabber/jabberClientAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  jabberClientAppDelegate.h
+//  clientJabber
+//
+//  Created by Jonathan Lequeux on 5/29/15.
+//  Copyright (c) 2015 Jonathan Lequeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface jabberClientAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ clientJabber/clientJabber/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  clientJabber
+//
+//  Created by Jonathan Lequeux on 5/29/15.
+//  Copyright (c) 2015 Jonathan Lequeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "jabberClientAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([jabberClientAppDelegate class]));
+    }
+}
